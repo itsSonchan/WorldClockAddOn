ff7887da8277f32383471aacd18e47a0bb195e51
TheSonchan <sonja.grund@icloud.com>
2025-01-23 18:20:31 +0100

    moved around some classes and ids for java to work, created Java code to update the cities' dates and times

--- script.js
+++ script.js
@@ -15,22 +15,22 @@ function showClock() {
         <option value="Europe/Berlin">Berlin</option>
         <option value="Australia/Sydney">Sydney</option>
       </select>
-      <div class="citygroup">
-        <div id="blockLeft">
-          <h2 id="city">Los Angeles</h2>
-          <div id="date">August 15th 2024</div>
+      <div class="citygroup" id="berlin">
+        <div class="blockLeft">
+          <h2 class="city">Berlin</h2>
+          <div class="date">August 15th 2024</div>
         </div>
-        <div id="block-right">
-          <div id="time">1:34:23</div>
+        <div class="block-right">
+          <div class="time">1:34:23</div>
         </div>
       </div>
-      <div class="citygroup">
-        <div id="blockLeft">
-          <h2 id="city">Tokyo</h2>
-          <div id="date">August 15th 2024</div>
+      <div class="citygroup" id="tokyo">
+        <div class="blockLeft">
+          <h2 class="city" >Tokyo</h2>
+          <div class="date">August 15th 2024</div>
         </div>
-        <div id="block-right">
-          <div id="time">1:34:23</div>
+        <div class="block-right">
+          <div class="time">1:34:23</div>
         </div>
       </div>
       <footer>
@@ -40,6 +40,21 @@ function showClock() {
     </div>
     `;
   attachTinyTardisListener();
+
+  function updateClock() {
+    let berlin = document.querySelector("#berlin");
+    let berlinDate = berlin.querySelector(".date");
+    berlinDate.innerHTML = moment.tz("Europe/Berlin").format("MMMM Do YYYY");
+    let berlinTime = berlin.querySelector(".time");
+    berlinTime.innerHTML = moment.tz("Europe/Berlin").format("HH:mm:ss");
+
+    let tokyo = document.querySelector("#tokyo");
+    let tokyoDate = tokyo.querySelector(".date");
+    tokyoDate.innerHTML = moment.tz("Asia/Tokyo").format("MMMM Do YYYY");
+    let tokyoTime = tokyo.querySelector(".time");
+    tokyoTime.innerHTML = moment.tz("Asia/Tokyo").format("HH:mm:ss");
+  }
+  setInterval(updateClock, 1000);
 }
 function attachTinyTardisListener() {
   let tinyTardis = document.querySelector("#tiny-tardis");
